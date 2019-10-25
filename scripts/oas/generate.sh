c2e9ef049e3cf0a1a4da575a3ff0b82b78528c0f
Zafer Genckaya <zafer@opsgenie.com>
2019-10-25 12:13:00 +0300

    Update Opsgenie OAS file generation script to be aligned with the master of opsgenie-oas project

--- scripts/oas/generate.sh
+++ scripts/oas/generate.sh
@@ -11,5 +11,5 @@ done
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd "${OPSGENIE_OAS}"
-node "${OPSGENIE_OAS}/multi-file-swagger/index.js" -- "${OPSGENIE_OAS}/swagger.yaml" > "${DIR}/../../opsgenie-oas.json" -- alert incident heartbeat account
+node "${OPSGENIE_OAS}/multi-file-swagger/index.js" "${OPSGENIE_OAS}/swagger.yaml" alert incident heartbeat account > "${DIR}/../../opsgenie-oas.json"
 cd "${DIR}"
