0b36642fb022796bbcac79ec068287b15d0e97a1
M.Hamza Khan <mkhan@atlassian.com>
2020-11-26 21:37:56 +0300

    update urllib3 version to 1.26.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import os
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "opsgenie-sdk"
-VERSION = "2.0.3"
+VERSION = "2.1.0"
 # To install the library, run the following
 #
 # python setup.py install
@@ -24,7 +24,7 @@ VERSION = "2.0.3"
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 
-REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil", "numpy >= 1.16.3", "setuptools >= 21.0.0", "tenacity >= 5.0.4"]
+REQUIRES = ["urllib3 >= 1.26.2", "six >= 1.10", "certifi", "python-dateutil", "numpy >= 1.16.3", "setuptools >= 21.0.0", "tenacity >= 5.0.4"]
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
