0d20d2314522fc0fd8ca5f0faa16f7c96387e123
M.Hamza Khan <mkhan@atlassian.com>
2021-08-02 15:30:07 +0200

    upgrade sdk version

--- opsgenie_sdk/__init__.py
+++ opsgenie_sdk/__init__.py
@@ -168,7 +168,7 @@ from opsgenie_sdk.models.weekday_time_restriction import WeekdayTimeRestriction
 from opsgenie_sdk.models.weekday_time_restriction_interval import WeekdayTimeRestrictionInterval
 from opsgenie_sdk.models.weekday_time_restriction_interval_all_of import WeekdayTimeRestrictionIntervalAllOf
 
-__version__ = "2.1.4"
+__version__ = "2.1.5"
 
 # import apis into sdk package
 
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import os
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "opsgenie-sdk"
-VERSION = "2.1.4"
+VERSION = "2.1.5"
 # To install the library, run the following
 #
 # python setup.py install
